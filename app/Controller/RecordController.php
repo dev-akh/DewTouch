45f2dc7f9c8b752e2f42cc7fcf369b1705d5ab5f
Aung Kyaw Htwe <dev.aungkyawhtwe@gmail.com>
2022-10-01 21:55:28 +0630

    complete :+1:  use datatable in service for question 1

--- app/Controller/RecordController.php
+++ app/Controller/RecordController.php
@@ -1,21 +1,35 @@
 <?php
 	class RecordController extends AppController{
-		
+
+		public $components = array('DataTable');
+
 		public function index(){
 			ini_set('memory_limit','256M');
 			set_time_limit(0);
 			
 			$this->setFlash('Listing Record page too slow, try to optimize it.');
 			
-			
-			$records = $this->Record->find('all');
-			
-			$this->set('records',$records);
-			
-			
 			$this->set('title',__('List Record'));
 		}
-		
+
+		public function getData()
+		{
+			$this->layout = false; 
+			$this->render(false);
+			if($this->request->is('ajax')) {
+				$this->paginate = array(
+					'fields' => array('id', 'name'),
+					'link' => array(
+						'Record' => array(
+							'fields' => array('id','name')
+						)
+					)
+				);
+				$this->DataTable->mDataProp = true;
+				$records=$this->DataTable->getResponse();
+				echo json_encode($records);exit;
+			}
+		}
 		
 // 		public function update(){
 // 			ini_set('memory_limit','256M');
