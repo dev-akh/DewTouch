c72a9e2814b63e8a647cf2c787ef7bf23ec6752b
Aung Kyaw Htwe <dev.aungkyawhtwe@gmail.com>
2022-10-02 14:50:34 +0630

    config : limit transation records

--- app/Controller/TransactionController.php
+++ app/Controller/TransactionController.php
@@ -4,12 +4,16 @@ class TransactionController extends AppController{
 
     public function index()
     {
+        ini_set('memory_limit','-1');
+		set_time_limit(0);
+
         $this->setFlash('MVC');
         $this->loadModel('Transaction');
 		$transactions = $this->Transaction->find('all',
                                     array(
                                         'conditions' => array('Transaction.valid' => 1),
-                                        'recursive'=>1
+                                        'recursive'=>1,
+                                        'limit'=>500
                                     )
                                 );
         // debug($transactions);exit;
